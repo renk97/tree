0053b60c5ad976feeef18b6c0cf4f997688e9e6f
starbit_ren <ren.kung@star-bit.io>
2023-09-08 08:27:25 +0000

    split array

--- main.go
+++ main.go
@@ -5,11 +5,14 @@ import (
 	"log"
 	"net/http"
 	"os"
+	"strings"
 
 	"github.com/gin-gonic/gin"
 	_ "github.com/joho/godotenv/autoload"
 	"gorm.io/driver/mysql"
 	"gorm.io/gorm"
+	"gorm.io/gorm/logger"
+	"gorm.io/gorm/schema"
 )
 
 var db *gorm.DB
@@ -20,6 +23,12 @@ type Tree struct {
 	Struct string
 }
 
+type OTree struct {
+	Id     int
+	Root   string
+	Struct []string
+}
+
 func init() {
 	var err error
 	dsn := fmt.Sprintf("%s:%s@tcp(%s)/%s?charset=utf8mb4&parseTime=True&loc=Local",
@@ -27,9 +36,15 @@ func init() {
 		os.Getenv("DBPASSWORD"),
 		os.Getenv("DBHOST")+":"+os.Getenv("DBPORT"),
 		os.Getenv("DBNAME"))
-	db, err := gorm.Open(mysql.Open(dsn), &gorm.Config{})
+	db, err = gorm.Open(mysql.Open(dsn), &gorm.Config{
+		Logger: logger.Default.LogMode(logger.Info),
+		NamingStrategy: schema.NamingStrategy{
+			SingularTable: true,
+		},
+	})
 
 	fmt.Println(dsn)
+	fmt.Printf("%+v\n", db)
 
 	if err != nil {
 		log.Println(err)
@@ -61,21 +76,26 @@ func GetTreeRouter(c *gin.Context) {
 	})
 }
 
-func GetTreeController() (Tree, int) {
+func GetTreeController() (OTree, int) {
 	code := http.StatusOK
 
 	resp, err := GetTreeModel()
-	if err != nil {
-		code = http.StatusForbidden
+	struct_arr := strings.Split(resp.Struct, ",")
+	output := OTree{
+		Id:     resp.Id,
+		Root:   resp.Root,
+		Struct: struct_arr,
 	}
 
-	return resp, code
+	if err != nil {
+		code = http.StatusInternalServerError
+	}
+
+	return output, code
 }
 
-func GetTreeModel() (Tree, error) {
-	t := Tree{}
+func GetTreeModel() (t Tree, err error) {
+	err = db.Table("Mtree").Find(&t).Error
 
-	err := db.Table("test_tree").Find(&t).Error
-
-	return t, err
+	return
 }
